5e4acbcb37c0c6bf468c372147fb0e6975477a54
William Almeida <williamdssa@gmail.com>
2020-03-15 19:21:38 -0300

    initial

---
+++ src/main/java/com/william/api/GuiaSpringBoot/GuiaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.william.api.GuiaSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/william/api/GuiaSpringBoot/GuiaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.api.GuiaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
